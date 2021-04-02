688e1046a65dd43feb87018a72a519d3880c194f
Gabriel Paixão <gabrielpaixaoitp@gmail.com>
2021-04-02 17:06:09 -0300

    ex010-Conversor de Moedas.py

---
+++ ex010-Conversor de Moedas.py
@@ -0,0 +1,5 @@
+# Conversor de Moedas
+
+real = float(input("Quanto você tem na carteira? "))
+dolar = real / 5.63
+print("Com {:.2f} reias você consegue comprar {:.2f} dólares. ".format(real, dolar))
